8eee0f654a504daf1d7261eb1884fd6f21b1426c
plongtran239 <plongtran239@gmail.com>
2023-11-12 18:17:35 +0700

    Add footer component and update metadata and header styling

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -1,39 +1,16 @@
-import { Button } from '@/components/ui/button';
-import { Phone, Mail, Twitter, Facebook, Instagram } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 function Header() {
     const router = useRouter();
 
     return (
-        <header className='w-full'>
-            {/* Top Bar */}
-            <div className='flex items-center justify-between bg-[#474747] px-32 py-2 text-white'>
-                {/* Contact */}
-                <div className='flex'>
-                    <div className='flex items-center'>
-                        <Phone size={16} />
-                        <p className='pl-1'>0813993353</p>
-                    </div>
-                    <div className='ml-4 flex items-center'>
-                        <Mail size={16} />
-                        <p className='pl-1'>cheesebistro.res@gmail.com</p>
-                    </div>
-                </div>
-
-                {/* Social */}
-                <div className='flex w-16 justify-between'>
-                    <Twitter size={16} />
-                    <Facebook size={16} />
-                    <Instagram size={16} />
-                </div>
-            </div>
-
+        <header className='sticky top-0 z-10 w-full bg-white'>
             {/* Header */}
-            <div className='flex items-center justify-between bg-red-50 px-32 py-5'>
+            <div className='flex items-center justify-between px-32 py-5'>
                 {/* Logo */}
-                <div className='flex'>
+                <div className='flex items-start'>
                     <Image
                         src='/cheese-logo.png'
                         alt='cheese-logo'
@@ -45,10 +22,39 @@ function Header() {
                     </p>
                 </div>
 
+                {/* Navbar */}
+                <nav className='flex items-center space-x-8'>
+                    {/* Active */}
+                    <a
+                        href='#home'
+                        className='rounded-full px-6 py-2 text-base font-semibold hover:bg-[#eceeea] active:bg-[#9ca584] active:text-white'
+                    >
+                        Home
+                    </a>
+                    <a
+                        href='#services'
+                        className='rounded-full px-6 py-2 text-base font-semibold hover:bg-[#eceeea] active:bg-[#9ca584] active:text-white'
+                    >
+                        Services
+                    </a>
+                    <a
+                        href='#about'
+                        className='rounded-full px-6 py-2 text-base font-semibold hover:bg-[#eceeea] active:bg-[#9ca584] active:text-white'
+                    >
+                        About
+                    </a>
+                    <a
+                        href='#contact'
+                        className='rounded-full px-6 py-2 text-base font-semibold hover:bg-[#eceeea] active:bg-[#9ca584] active:text-white'
+                    >
+                        Contact
+                    </a>
+                </nav>
+
                 {/* Actions */}
                 <div className='flex'>
                     <Button
-                        className='rounded-3xl'
+                        className='rounded-3xl bg-white text-black outline outline-1 outline-black hover:bg-black hover:text-white'
                         onClick={() => {
                             router.push('/sign-in');
                         }}
