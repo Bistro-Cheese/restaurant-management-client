2e51e12e7b669da6659c6e77d7a8e53ee4421437
ngminhat187 <ngminhat187@gmail.com>
2023-11-16 14:17:35 +0700

    #4 - clean code and add Create User

--- src/app/(page)/(root)/(admin)/owner/(employees)/employees/[employeeId]/page.tsx
+++ src/app/(page)/(root)/(admin)/owner/(employees)/employees/[employeeId]/page.tsx
@@ -1,5 +1,21 @@
-const EmployeePage = () => {
-    return <div>Employee Page</div>;
+'use client';
+
+import { UserForm } from '../../../_components/create-employee-form';
+
+interface UserPageProps {
+    params: {
+        employeeId: string;
+    };
+}
+
+const UserPage: React.FC<UserPageProps> = ({ params }) => {
+    return (
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                <UserForm userId={params.employeeId} />
+            </div>
+        </div>
+    );
 };
 
-export default EmployeePage;
+export default UserPage;
